952afde7dc3f9284d607c4076a25f65af73b49d7
PaildarBasanthi <basanthisrec@gmail.com>
2023-08-31 11:59:22 +0530

    first Commit

---
+++ packages/SQLQuery1.sql
@@ -0,0 +1,13 @@
+drop database PlayersDb
+create database PlayersDb
+use PlayersDb
+
+create table Player
+(PlayerId int primary key,
+FirstName nvarchar(50) not null,
+LastName nvarchar(50) not null,
+JerseyNumber int not null,
+Position nvarchar(50) not null,
+Team nvarchar(50) not null)
+
+insert into Player values (1, 'Dhoni', 'MS', 11, 'Keeper', 'CSK')
